228311d27b217e6cb2210ddb08cbe87e947db27a
Natalia Lipatova <ndlipatova@mts.ru>
2023-10-12 21:02:26 +0300

    fix scroll

--- src/app/layouts/admin/admin-layout.component.ts
+++ src/app/layouts/admin/admin-layout.component.ts
@@ -27,40 +27,40 @@ export class AdminLayoutComponent implements OnInit {
     }
 
     ngOnInit() {
-      const elemMainPanel = <HTMLElement>document.querySelector('.main-panel');
-      const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper');
+      // const elemMainPanel = <HTMLElement>document.querySelector('.main-panel');
+      // const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper');
 
-       this.router.events.subscribe((event:any) => {
-          if (event instanceof NavigationStart) {
-             if (event.url != this.lastPoppedUrl)
-                 this.yScrollStack.push(window.scrollY);
-         } else if (event instanceof NavigationEnd) {
-             if (event.url == this.lastPoppedUrl) {
-                 this.lastPoppedUrl = undefined;
-                 window.scrollTo(0, this.yScrollStack.pop());
-             }
-             else
-                 window.scrollTo(0, 0);
-         }
-      });
-      this._router = this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
-           elemMainPanel.scrollTop = 0;
-           elemSidebar.scrollTop = 0;
-      });
+      //  this.router.events.subscribe((event:any) => {
+      //     if (event instanceof NavigationStart) {
+      //        if (event.url != this.lastPoppedUrl)
+      //            this.yScrollStack.push(window.scrollY);
+      //    } else if (event instanceof NavigationEnd) {
+      //        if (event.url == this.lastPoppedUrl) {
+      //            this.lastPoppedUrl = undefined;
+      //            window.scrollTo(0, this.yScrollStack.pop());
+      //        }
+      //        else
+      //            window.scrollTo(0, 0);
+      //    }
+      // });
+      // this._router = this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+      //      elemMainPanel.scrollTop = 0;
+      //      elemSidebar.scrollTop = 0;
+      // });
 
-        this._router = this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(event => {
-        //   this.url = event.url;
-          // this.navbar.sidebarClose();
-        });
+      //   this._router = this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(event => {
+      //   //   this.url = event.url;
+      //     // this.navbar.sidebarClose();
+      //   });
 
-        var isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
-        if (isWindows){
+        // var isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
+        // if (isWindows){
         // if we are on windows OS we activate the perfectScrollbar function
             var $main_panel = $('.main-panel');
             $main_panel.perfectScrollbar();
-        }
-
+        // }
     }
+    
     public isMap(){
         if(this.location.prepareExternalUrl(this.location.path()) == '#/maps/fullscreen'){
             return true;
